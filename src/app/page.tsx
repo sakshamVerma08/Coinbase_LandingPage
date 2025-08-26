5d96dd0c5c8920caaeb43c0d6011490eaea67442
root <ssharma1@wesee.in>
2025-08-26 13:16:14 +0530

    Made the navbar, starting with the hero section , added necessary images and fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
 import { DatePickerDemo } from "import/components/ui/date-picker";
 import { MyChart } from "import/components/ui/main-chart";
+import Image from "next/image";
 
 export default function Home(){
 
   return (
 
-    <div>
-        
-    </div>
+    <>
+      <section id = "hero-section" className="w-full bg-purple-200 h-auto flex justify-between items-center p-20">
+
+        <div id="namasteIndia" className="bg-amber-200">
+
+          <h1 className="text-6xl font-medium tracking-normal leading-18">Namaste,India!<br/>Meet Coinbase 🇮🇳</h1>
+        </div>
+
+        <div id="tradingImage">
+
+          <Image src = "/images/landing_page_img1.avif" alt = "Trading_image" width={500} height = {500}/>
+        </div>
+
+      </section>
+    </>
   );
 }
