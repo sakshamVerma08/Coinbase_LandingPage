bed4405bd4633b84588461ab56929241f4ff751c
Saksham Verma <sakshamverma1000@gmail.com>
2025-08-27 02:22:28 +0530

    Defined custom fonts (font1, font2 & font3) in custom_fonts.css, Created the landing_hero.tsx component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "import/components/ui/button";
 import { DatePickerDemo } from "import/components/ui/date-picker";
-import LandingHero from "import/components/ui/global/landing_page/landing_hero";
-import LandingPage from "import/components/ui/global/landing_page/landing_page";
+import HeroSection from "import/components/ui/global/landing_page/landing_hero";
 import { Input } from "import/components/ui/input";
 import { MyChart } from "import/components/ui/main-chart";
 import Image from "next/image";
@@ -10,6 +9,6 @@ export default function Home(){
 
   return (
 
-    <LandingPage/>
+    <HeroSection/>
   );
 }
--- src/components/ui/global/landing_page/landing_hero.tsx
+++ src/components/ui/global/landing_page/landing_hero.tsx
@@ -1,29 +1,42 @@
 import React from 'react'
+import { Input } from '../../input';
+import { Button } from '../../button';
+import Image from 'next/image';
 
 type Props = {}
 
-const LandingHero = (props: Props) => {
+const HeroSection = (props: Props) => {
   return (
     <section id = "hero-section" className='min-h-1/2 h-1/2 min-w-screen flex flex-col items-center justify-around p-10'>
 
         <div id="namasteIndia">
 
-            <div>
-              <h2 className='text-4xl font-medium text-left mb-5 leading-12 '>Namaste, India!<br/>
+            <div className = "w-full text-nowrap">
+              <h2 className='text-5xl font-medium text-left mb-5 leading-12 font-font1 '>Namaste, India!<br/>
                   Meet Coinbase 🇮🇳</h2>
 
-              <aside className='text-slate-500 mb-3'>Coinbase is the world's safest and most trusted <br/>
+              <aside className='text-gray-500 mb-3 font-normal font-font3'>Coinbase is the world's safest and most trusted <br/>
                   platform to buy, sell and manage crypto.</aside>
 
-              <aside>We're launching soon! Add your email and you'll be the<br/>
+              <aside className='text-gray-500 mb-5 font-font3'>We're launching soon! Add your email and you'll be the<br/>
                       first to know when you can join.</aside>
               
             </div>
 
+            <div className='w-full'>
+
+              <Input height = {20} type = "email" placeholder='Enter your email' className = "border border-slate-600 rounded-sm p-8 text-2xl mb-5 w-4/4"/>
+              <Button type = "submit" className='bg-blue-600 text-white hover:bg-blue-700 rounded-full p-7 '>Get Notified</Button>
+            </div>
+
+        </div>
+
+        <div id="firstImage" className='mt-10'>
+          <Image src = "/images/landing_page_img1.avif" alt = "Image1" height = {400} width={300}/>
         </div>
 
     </section>
   )
  }
 
-export default LandingHero;
+export default HeroSection;
